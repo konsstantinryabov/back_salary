1e80f30053e4904743eff6e90bc49168e81d867d
Константин Рябов <konsstantinryabov@gmail.com>
2023-07-14 21:00:09 +0400

    БД

--- salary/salary/settings.py
+++ salary/salary/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = 'salary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'salary',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'salary',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
